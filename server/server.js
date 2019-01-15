5e482f2fa5662079f91d3f828b42668780da9fc7
Vasilis <papakonv7@gmail.com>
2019-01-15 23:12:54 +0200

    Initialize Socke io connection

--- server/server.js
+++ server/server.js
@@ -1,18 +1,28 @@
 const path = require('path');
+const http = require('http');
 
 const express = require('express');
+const socketIO = require('socket.io');
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 const publicPath = path.join(__dirname + './../public');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.get('/', (req, res)=> {
-   res.send('index.html'); 
+io.on('connection', (socket)=> {
+   console.log('New user connected');
+    
+   socket.on('disconnect', ()=> {
+    console.log('User was disconnected');
+  });  
 });
 
-app.listen(PORT, ()=> {
+
+
+server.listen(PORT, ()=> {
    console.log(`App listen to port ${PORT}`); 
 });
