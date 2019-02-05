f9f744f1c87552991b487c184d970df28f50bad1
Vasilis <papakonv7@gmail.com>
2019-02-05 21:49:27 +0200

    Vue js corrected

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,52 @@ socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
 
+ socket.on('newMessage',function (message) {
+        appSocket.incomings.push(message);
+        })
+        
+        
+    new Vue({
+        el: '#app',
+        data: {
+         message: 'Welcome to the chat App!'   
+        }
+    })
+        
+    
+        
+    new Vue ({
+        el: '#chatapp',
+        data: {
+            msg: '',
+        },
+        methods: {
+            submit: function (e) {
+                e.preventDefault()
+                
+               socket.emit('createMessage', {
+                   from: 'Bob',
+                   text: this.msg
+               }, function (data) {
+                    console.log('Got it', data);
+                   })
+                 this.msg=''
+                  }
+        }
+       })
+        
+  var appSocket = new Vue ({
+        el: '#socket_on',
+        data: {
+            incomings : []
+        },
+        methods: {
+            
+      }   
+    })
+    
+    
+
 /*
 socket.emit('createMessage', {
     from,
